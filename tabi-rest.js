388fbfacfa3a780832928ec9bed724967c97c8a7
p-kadono <78052813+p-kadono@users.noreply.github.com>
2021-01-28 15:31:58 +0900

    Add files via upload

---
+++ tabi-rest.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    $('.login-form').fadeIn(1000);
+
+    $(window).scroll(function (){
+		$('.fadein').each(function(){
+			var elemPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight + 200){
+				$(this).addClass('scrollin');
+			}
+		});
+	});
+
+});
